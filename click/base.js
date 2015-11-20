dd56b6e4c04559f62a6fb96474c380b36e2642df
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2015-11-20 09:19:03 -0800

    Finished click

--- click/base.js
+++ click/base.js
@@ -1,8 +1,8 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
-	$("span").on("click", function clickAndAdd(event){
-	  var current_text = $("h2").text()
-	  $("h2").text( current_text + " (this)" )
-	});
-});
+//$(document).ready(function(){
+	//$("span").on("click", function clickAndAdd(event){
+	  //var current_text = $("h2").text()
+	  //$("h2").text( current_text + " (this)" )
+	//});
+//});
