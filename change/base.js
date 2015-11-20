8db9c9b2c3d50d0eb00efbef8bbe7df3c29bd584
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2015-11-19 17:23:39 -0800

    Tried number 3

--- change/base.js
+++ change/base.js
@@ -1,7 +1,7 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+	$(#total).text(#left + #right) //This doesn't work, I think I might have to add some variables, not sure how to get the numbers to be actual values.
   $("#time").text( Date.now() );
 
 })
